709f8b1b62b3ff4c70cd0b6379b1a2fbd14a854f
Graham <gleachkr@gmail.com>
2025-02-11 08:56:18 -0500

    Switch to crane, make libsql available

--- flake.nix
+++ flake.nix
@@ -1,22 +1,31 @@
 {
   inputs = {
-    naersk.url = "github:nix-community/naersk/master";
+    crane.url = "github:ipetkov/crane";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
+  outputs = { self, nixpkgs, utils, crane }:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
+        cranelib = crane.mkLib pkgs;
       in
       {
-        defaultPackage = naersk-lib.buildPackage ./.;
+
+        defaultPackage = cranelib.buildPackage { 
+          src = cranelib.cleanCargoSource ./.; 
+          buildInputs = [
+            pkgs.pkg-config
+            pkgs.openssl
+          ];
+        };
+
         devShell = with pkgs; mkShell {
           buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
+
       }
     );
 }
