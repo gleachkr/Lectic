6e2d3565004ebed7fe303e6e4991a3595f2ee394
Graham <gleachkr@gmail.com>
2025-03-30 10:56:21 -0400

    Switch to inheriting from plain markdown
    We're now using commonmark as the basic markdown dialect

--- extra/lectic.nvim/ftdetect/lectic.lua
+++ extra/lectic.nvim/ftdetect/lectic.lua
@@ -1,11 +1,6 @@
-local parent_ft = "markdown"
-if vim.filetype.match({ filename = "test.pandoc" }) == "pandoc" then
-    parent_ft = "pandoc"
-end
-
 vim.filetype.add({
     extension = {
-        lec = 'lectic.' .. parent_ft,
-        lectic = 'lectic.' .. parent_ft
+        lec = 'lectic.markdown',
+        lectic = 'lectic.markdown'
     }
 })
