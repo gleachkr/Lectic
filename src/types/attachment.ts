ca9f8b2b1d3b8369a4b123eafe641681dff2d61b
Graham <gleachkr@gmail.com>
2025-04-24 21:24:11 -0400

    Add support for MCP resources as a link schema

--- src/types/attachment.ts
+++ src/types/attachment.ts
@@ -1,10 +1,13 @@
 import type { MessageLink } from "./message"
 import type { BunFile } from "bun"
 import { Glob } from "bun"
+import { MCPTool } from "../tools/mcp.ts"
+import type { ReadResourceResult } from "@modelcontextprotocol/sdk/types.js"
 
 export class MessageAttachment {
     file : BunFile | undefined
     response : Promise<Response> | undefined
+    resource : Promise<ReadResourceResult> | undefined
     title : string
     URI : string
 
@@ -20,7 +23,16 @@ export class MessageAttachment {
                     this.response = Bun.fetch(link.URI); break
                 }
                 case "s3:" : {
-                    this.response = Bun.fetch(link.URI)
+                    this.response = Bun.fetch(link.URI); break
+                }
+                default : {
+                    const mcp_match = /^([^+]+)\+/.exec(url.protocol)
+                    if (mcp_match && mcp_match[1] in MCPTool.client_registry) {
+                        MCPTool.client_registry[mcp_match[1]]
+                        this.resource = MCPTool.client_registry[mcp_match[1]].readResource({
+                            uri: link.URI.slice(mcp_match[1].length+1)
+                        })
+                    }
                 }
             }
         } catch {
@@ -31,22 +43,26 @@ export class MessageAttachment {
     }
 
     async exists() : Promise<boolean> {
+        if (this.resource) return true
         if (this.response) return true
         if (this.file?.exists()) return true
         return false
     }
 
-    async getType() {
+    async getType() : Promise<string | null > {
         if (this.file) {
             return this.file.type.replace(/^text\/.+$/,"text/plain")
         } else if (this.response) {
             const headers = (await this.response).headers
-            return headers.get("Content-Type")?.replace(/^text\/.+$/,"text/plain")
-        } 
+            return headers.get("Content-Type")?.replace(/^text\/.+$/,"text/plain") ?? null
+        } else if (this.resource) {
+            const resource = (await this.resource) 
+            return resource.contents[0].mimeType || "application/octet-stream"
+        }
         return null
     }
 
-    async getBytes() {
+    async getBytes() : Promise<Uint8Array | null>{
         if (this.file) {
             return this.file.bytes()
         } else if (this.response) {
@@ -54,7 +70,14 @@ export class MessageAttachment {
             // recognize Bun's Response.bytes()
             return (await (await this.response).blob()).bytes()
             // TODO error handling in case of a failed fetch
-        } 
+        } else if (this.resource) {
+            const contents = (await this.resource).contents[0]
+            if (contents.blob) {
+                return Uint8Array.fromBase64(contents.blob as string)
+            } else {
+                return Buffer.from(contents.text as string)
+            }
+        }
         return null
     }
 
