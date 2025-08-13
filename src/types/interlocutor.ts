f99440982e7c9a45cb0b2c54b572ab9f7a6c6f6a
Graham <gleachkr@gmail.com>
2025-08-12 22:22:19 -0400

    Fix minor fallthrough issue in interlocutor validation
    Previously, if temperature was present, a malformed tool field would be
    ignored by the validation

--- src/types/interlocutor.ts
+++ src/types/interlocutor.ts
@@ -19,33 +19,44 @@ export type Interlocutor = {
 export function validateInterlocutor(raw : unknown) : raw is Interlocutor {
     if (typeof raw !== "object") {
         throw Error(`Interlocutor needs to be given with at least name and prompt fields. Got ${raw} instead.`)
-    } else if (raw === null) {
+    } 
+    if (raw === null) {
         throw Error("Something went wrong, got null for interlocutor")
-    } else if (!("name" in raw) || typeof raw.name !== "string") {
+    } 
+    if (!("name" in raw) || typeof raw.name !== "string") {
         throw Error("An interlocutor is missing a name. The name needs to be a string.")
-    } else if (!("prompt" in raw) || typeof raw.prompt !== "string") {
+    } 
+    if (!("prompt" in raw) || typeof raw.prompt !== "string") {
         throw Error(`Interlocutor ${raw.name} needs a prompt. The prompt needs to be a string.`)
-    } else if (("model" in raw) && typeof raw.model !== "string") {
+    } 
+    if (("model" in raw) && typeof raw.model !== "string") {
         throw Error(`The model type for ${raw.name} needs to be a string`)
-    } else if (("provider" in raw) && !isLLMProvider(raw.provider)) {
+    } 
+    if (("provider" in raw) && !isLLMProvider(raw.provider)) {
         throw Error(`The provider for ${raw.name} wasn't recognized.`)
-    } else if (("max_tokens" in raw) && typeof raw.max_tokens !== "number") {
+    } 
+    if (("max_tokens" in raw) && typeof raw.max_tokens !== "number") {
         // Check for positive natural number...
         throw Error(`The max_tokens for ${raw.name} wasn't well-formed, it needs to be a number.`)
-    } else if (("max_tool_use" in raw) && typeof raw.max_tool_use !== "number") {
+    } 
+    if (("max_tool_use" in raw) && typeof raw.max_tool_use !== "number") {
         // Check for positive natural number...
         throw Error(`The max_tool_use for ${raw.name} wasn't well-formed, it needs to be a number.`)
-    } else if (("reminder" in raw) && typeof raw.reminder !== "string") {
+    } 
+    if (("reminder" in raw) && typeof raw.reminder !== "string") {
         throw Error(`The reminder for ${raw.name} wasn't well-formed, it needs to be a string.`)
-    } else if (("nocache" in raw) && typeof raw.nocache!== "boolean") {
+    } 
+    if (("nocache" in raw) && typeof raw.nocache!== "boolean") {
         throw Error(`The nocache option for ${raw.name} wasn't well-formed, it needs to be a boolean.`)
-    } else if (("temperature" in raw)) {
+    } 
+    if (("temperature" in raw)) {
         if (typeof raw.temperature !== "number") {
             throw Error(`The temperature for ${raw.name} wasn't well-formed, it needs to be a number.`)
         } else if (raw.temperature > 1 || raw.temperature < 0) {
             throw Error(`The temperature for ${raw.name} wasn't well-formed, it needs to between 1 and 0.`)
         }
-    } else if (("tools" in raw)) {
+    } 
+    if (("tools" in raw)) {
         if (!(typeof raw.tools === "object" && raw.tools instanceof Array)) {
             throw Error(`The tools for ${raw.name} need to be given in an array.`)
         } else if (!(raw.tools.every(t => typeof t === "object"))) {
